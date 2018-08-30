71e660b2835b6eb996ae5bcf0878a02a31b41a41
Christopher Moore <christopher.e.moore@gmail.com>
2018-08-30 20:58:22 +0200

    made template a bit more modular

--- template.lua
+++ template.lua
@@ -1,12 +1,33 @@
 local table = require 'ext.table'
+local class = require 'ext.class'
 local showcode = require 'template.showcode'
+
+local DefaultOutput = class()
+function DefaultOutput:init()
+	self.strs = table()
+end
+function DefaultOutput:__call(str)
+	self.strs:insert(str)
+end
+function DefaultOutput:done()
+	return self.strs:concat()
+end
 
 --[[
 Lua template
 
+template(code, env, args)
+	text = text to process
+	env = environment of the code in the text to use
+	args = extra args
+		output = override output function 
+			During string processing, output(str) is called on each string block 'str'.
+			Once processing is finished, the result of template() is output:done(), or nil if output has no 'done' function.
+			The default 'output' accumulates strings and returns the concatenated results.
+
 usage: template(code, {a=1, b=2, ...})
 --]]
-local function template(code, args)
+local function template(code, args, funcArgs)
 	local argKeys, argValues = table(), table()
 	if args then
 		for k,v in pairs(args) do
@@ -14,15 +35,15 @@ local function template(code, args)
 			argValues:insert(v)
 		end
 	end
-	local outputFunc = '__output'
+	local outputFuncName = '__output'
 	local newcode = table{
-		'local '..table():append({outputFunc},argKeys):concat', '..' = ...\n',
+		'local '..table():append({outputFuncName},argKeys):concat', '..' = ...\n',
 	}
 	local function addprint(from,to)
 		local block = code:sub(from,to)
 		local eq = ('='):rep(5)	-- TODO make sure no such [=..=[ appears in the code block
 		local nl = block:find'\n' and '\n' or ''
-		newcode:insert(outputFunc..' ['..eq..'['..nl..block..']'..eq..']\n')
+		newcode:insert(outputFuncName..' ['..eq..'['..nl..block..']'..eq..']\n')
 	end
 	local pos = 1
 	while true do
@@ -42,7 +63,7 @@ local function template(code, args)
 			addprint(pos, start1-1)
 			local block = code:sub(start2+1, end1-1)
 			if ret then
-				newcode:insert(outputFunc..'(tostring('..block..'))\n')
+				newcode:insert(outputFuncName..'(tostring('..block..'))\n')
 			else
 				newcode:insert(block..'\n')
 			end
@@ -54,21 +75,17 @@ local function template(code, args)
 	newcode = newcode:concat()
 	local f, msg = load(newcode)
 	if not f then
-		print(showcode(newcode))
-		error(msg)
+		error('\n'..showcode(newcode)..'\n'..msg)
 	end
-	
-	local outputStrs = table()
+
+	local output = funcArgs and funcArgs.output or DefaultOutput()	
 	local result, msg = pcall(function()
-		f(function(str)
-			outputStrs:insert(str)
-		end, argValues:unpack())
+		f(output, argValues:unpack())
 	end)
 	if not result then
-		print(showcode(newcode))
-		error(msg)
+		error('\n'..showcode(newcode)..'\n'..msg)
 	end
-	return outputStrs:concat()
+	return output.done and output:done() or nil
 end
 
 return template
